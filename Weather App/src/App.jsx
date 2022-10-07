e9e4faf8d18a56f20ded8fa4d215d8870ec9e9e6
Дмитрий Троицкий <17783875+troitsky@users.noreply.github.com>
2022-10-08 00:18:31 +0300

    finished desktop css

--- Weather App/src/App.jsx
+++ Weather App/src/App.jsx
@@ -5,108 +5,129 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div class="App">
-      <div class="side_panel">
-        <div class='side_top_section'>
-          <button class="btn_search_location">Seach for places</button>
-          <button class="btn_circle get_user_location"><span class="material-symbols-outlined">my_location</span></button>
+    <div className="App">
+      <div className="side_panel">
+
+        {/* side panel info mode (deafult) */}
+        <div className='side_top_section'>
+          <button className="btn_search_location">Seach for places</button>
+          <button className="btn_circle get_user_location"><span className="material-symbols-outlined">my_location</span></button>
         </div>
-        <div class='weather_illustration_part'>
-          <img src="Cloud-background.png" class="weather_illustration_bg" alt="" />
-          <img src="/HeavyRain.png" alt="" class="weather_illustration_img" />
+        <div className='weather_illustration_part'>
+          <img src="Cloud-background.png" className="weather_illustration_bg" alt="" />
+          <img src="/HeavyRain.png" alt="" className="weather_illustration_img" />
         </div>
-        <p class="temprature_figure">15<span class='temp_unit'>°C</span></p>
-        <p class="weather_description">Shower</p>
-        <div class="side_lower_date">
-          <p class='displayed_weather_day'>Today</p>
-          <span className='date_divider'>•</span>
-          <p class='displayed_weather_date'>Fri, 5 Jun</p>
+        <p className="temprature_figure">15<span className='temp_unit'>°C</span></p>
+        <p className="weather_description">Shower</p>
+        <div className="side_lower_date">
+          <p className='displayed_weather_day'>Today</p>
+          <span classNameName='date_divider'>•</span>
+          <p className='displayed_weather_date'>Fri, 5 Jun</p>
         </div>
-        <p class="displayed_weather_location"><span class="material-symbols-outlined">location_on</span>Helsinki</p>
+        <p className="displayed_weather_location"><span className="material-symbols-outlined">location_on</span>Helsinki</p>
+
+      {/* side panel info mode (search mode) */}
+      {/* <span class="side_close_icon material-symbols-outlined">close</span>
+      <div className='side_top_search_section'>
+          <span class="search_icon material-symbols-outlined">search</span>
+          <input type="text" className='location_input' placeholder='search for places' />
+          <button className="btn_search">Search</button>
       </div>
-      <main class="weather_details">
-        <div class="main_container">
-          <div class="unit_selection_btns">
-            <button class='btn_circle btn_unit_selection'>°C</button>
-            <button class='btn_circle btn_unit_selection'>°F</button>
+      <ul className="search_results">
+        <li className="search_result">london<span class="material-symbols-outlined">navigate_next</span></li>
+        <li className="search_result">london<span class="material-symbols-outlined">navigate_next</span></li>
+        <li className="search_result">london<span class="material-symbols-outlined">navigate_next</span></li>
+      </ul> */}
+
+      </div>
+
+      
+
+      <main className="weather_details">
+        <div className="main_container">
+          <div className="unit_selection_btns">
+            <button className='btn_circle btn_unit_selection'>°C</button>
+            <button className='btn_circle btn_unit_selection'>°F</button>
           </div>
-          <div class="five_days_forecast_part">
-            <div class="weather_sm_card">
-              <p class="forecast_day">Sun, 7 Jun</p>
-              <img src="/LightRain.png" alt="" class='forecast_sm_img'/>
-              <div class="forecast_minmax">
-                <p class="forecast_max_temp">16°C</p>
-                <p class="forecast_min_temp">11°C</p>
+          <div className="five_days_forecast_part">
+            <div className="weather_sm_card">
+              <p className="forecast_day">Sun, 7 Jun</p>
+              <img src="/LightRain.png" alt="" className='forecast_sm_img'/>
+              <div className="forecast_minmax">
+                <p className="forecast_max_temp">16°C</p>
+                <p className="forecast_min_temp">11°C</p>
               </div>
             </div>
-            <div class="weather_sm_card">
-              <p class="forecast_day">Sun, 7 Jun</p>
-              <img src="/LightRain.png" alt="" class='forecast_sm_img'/>
-              <div class="forecast_minmax">
-                <p class="forecast_max_temp">16°C</p>
-                <p class="forecast_min_temp">11°C</p>
+            <div className="weather_sm_card">
+              <p className="forecast_day">Sun, 7 Jun</p>
+              <img src="/LightRain.png" alt="" className='forecast_sm_img'/>
+              <div className="forecast_minmax">
+                <p className="forecast_max_temp">16°C</p>
+                <p className="forecast_min_temp">11°C</p>
               </div>
             </div>
-            <div class="weather_sm_card">
-              <p class="forecast_day">Sun, 7 Jun</p>
-              <img src="/LightRain.png" alt="" class='forecast_sm_img'/>
-              <div class="forecast_minmax">
-                <p class="forecast_max_temp">16°C</p>
-                <p class="forecast_min_temp">11°C</p>
+            <div className="weather_sm_card">
+              <p className="forecast_day">Sun, 7 Jun</p>
+              <img src="/LightRain.png" alt="" className='forecast_sm_img'/>
+              <div className="forecast_minmax">
+                <p className="forecast_max_temp">16°C</p>
+                <p className="forecast_min_temp">11°C</p>
               </div>
             </div>
-            <div class="weather_sm_card">
-              <p class="forecast_day">Sun, 7 Jun</p>
-              <img src="/LightRain.png" alt="" class='forecast_sm_img'/>
-              <div class="forecast_minmax">
-                <p class="forecast_max_temp">16°C</p>
-                <p class="forecast_min_temp">11°C</p>
+            <div className="weather_sm_card">
+              <p className="forecast_day">Sun, 7 Jun</p>
+              <img src="/LightRain.png" alt="" className='forecast_sm_img'/>
+              <div className="forecast_minmax">
+                <p className="forecast_max_temp">16°C</p>
+                <p className="forecast_min_temp">11°C</p>
               </div>
             </div>
-            <div class="weather_sm_card">
-              <p class="forecast_day">Sun, 7 Jun</p>
-              <img src="/LightRain.png" alt="" class='forecast_sm_img'/>
-              <div class="forecast_minmax">
-                <p class="forecast_max_temp">16°C</p>
-                <p class="forecast_min_temp">11°C</p>
+            <div className="weather_sm_card">
+              <p className="forecast_day">Sun, 7 Jun</p>
+              <img src="/LightRain.png" alt="" className='forecast_sm_img'/>
+              <div className="forecast_minmax">
+                <p className="forecast_max_temp">16°C</p>
+                <p className="forecast_min_temp">11°C</p>
               </div>
             </div>
           </div>
-            <h3 class='weather_highlights_title'>Today’s Hightlights</h3>
-            <div class="weather_highlights_part">
+            <h3 className='weather_highlights_title'>Today’s Hightlights</h3>
+            <div className="weather_highlights_part">
               
-              <div class="highligt_card">
-                <p class="highlight_title">Wind status</p>
-                <p class="highlight_figure">7<span class='highlight_unit'>mph</span></p>
-                <div class="wind_direction_part">
-                  <div class="wind_direction_bg">
-                  <span class="material-symbols-outlined">navigation</span>
+              <div className="highligt_card">
+                <p className="highlight_title">Wind status</p>
+                <p className="highlight_figure">7<span className='highlight_unit'>mph</span></p>
+                <div className="wind_direction_part">
+                  <div className="wind_direction_bg">
+                  <span className="material-symbols-outlined">navigation</span>
                   </div>
-                  <p class='wind_direction_text'></p>
+                  <p className='wind_direction_text'>WSW</p>
                 </div>
               </div>
               
-              <div class="highligt_card">
-                <p class="highlight_title">Humidity</p>
-                <p class="highlight_figure">84<span class='highlight_unit'>%</span></p>
-                <div class="humidity_meter_part">
-                  <span class='humidity_value_marker'>0</span>
-                  <span class='humidity_value_marker'>50</span>
-                  <span class='humidity_value_marker'>100</span>
-                  <div class="humidity_meter_bar"></div>
-                  <div class="humidity_meter_bg"></div>
-                  <span class='humidity_value_unit'>%</span>
+              <div className="highligt_card">
+                <p className="highlight_title">Humidity</p>
+                <p className="highlight_figure">84<span className='highlight_unit'>%</span></p>
+                <div className="humidity_meter_part">
+                  <div classNameName="markers_row">
+                    <p className='humidity_value_marker'>0</p>
+                    <p className='humidity_value_marker'>50</p>
+                    <p className='humidity_value_marker'>100</p>
+                  </div>                 
+                  <div className="humidity_meter_bar"></div>
+                  <div className="humidity_meter_bg"></div>
+                  <span className='humidity_value_unit'>%</span>
                 </div>
               </div>
 
-              <div class="highligt_card">
-                <p class="highlight_title">Air pressure</p>
-                <p class="highlight_figure">998<span class='highlight_unit'>mb</span></p>
+              <div className="highligt_card">
+                <p className="highlight_title">Air pressure</p>
+                <p className="highlight_figure">998<span className='highlight_unit'>mb</span></p>
               </div>
 
-              <div class="highligt_card">
-                <p class="highlight_title">Visibility</p>
-                <p class="highlight_figure">6<span class='highlight_unit'>miles</span></p>
+              <div className="highligt_card">
+                <p className="highlight_title">Visibility</p>
+                <p className="highlight_figure">6,4 <span className='highlight_unit'>miles</span></p>
               </div>
 
             </div>
